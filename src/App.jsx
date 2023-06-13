696cb89cd47c045cf13387f60c3f06b385da409b
devEliasJr <eliasengmecanica@gmail.com>
2023-06-13 15:23:55 -0300

    feat: implementação do contexto na aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 //router-dom
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+//Context
+import { AuthProvider } from "./context/authContext";
 
 //Style - CSS
 import "./App.css";
@@ -9,7 +12,7 @@ import Navigation from "./componets/Navigation";
 import Footer from "./componets/Footer";
 
 //Pages
-import ProductsCards from "./componets/products-Cards"
+import ProductsCards from "./componets/products-Cards";
 import ProductDescription from "./pages/ProductDescription";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
@@ -19,24 +22,27 @@ import Dashboard from "./pages/Dashboard/Dashboard";
 import NewProducts from "./pages/NewProducts/NewProducts";
 import EditPost from "./pages/EditPost/EditPost";
 
-
-
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/about" element={<Dashboard />} />
-          <Route path="/product-register" element={<NewProducts />} />
-          <Route path="/product-edit/:id" element={<EditPost />} />
-          <Route path="/product-description/:id/:name" element={<ProductDescription />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/about" element={<Dashboard />} />
+            <Route path="/product-register" element={<NewProducts />} />
+            <Route path="/product-edit/:id" element={<EditPost />} />
+            <Route
+              path="/product-description/:id/:name"
+              element={<ProductDescription />}
+            />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
